1b05b12b759a06757d50f2d79985595cd661124c
Sourav Chakraborty <cy.sourav@gmail.com>
2023-04-23 17:37:33 +0530

    Fixed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open('pt.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 def predict(aqi):
     prediction = model.predict(aqi)
